d46843d3ca710c0cc564a7d73bc40c006b177fe8
abiolaede <abiola2m4ac@gmail.com>
2022-09-08 15:06:49 -0700

    commit commplier

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ *print the string with the put function
+ *description: using the main function
+ *return :0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
