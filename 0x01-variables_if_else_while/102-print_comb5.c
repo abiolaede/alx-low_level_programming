c13c1964265fbd464755b92b90a9510c69ffe0ff
abiolaede <abiola2m4ac@gmail.com>
2022-09-12 11:41:48 -0700

    updated variable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int num1, num2
 
-	for(num1=0;num1<=98;num1++)
+	for(num1=0; num1<=98; num1++)
 	{
-		for(num2=num1+1;num2<=99;num2++)
+		for(num2=num1+1; num2<=99; num2++)
 		{
 		putchar((num1 / 10)+'0');
 		putchar((num1 % 10)+'0');
@@ -24,5 +24,5 @@ int main(void)
 		}
 	}
 	putchar(\n);
-	return (1);
+	return (0);
 }
