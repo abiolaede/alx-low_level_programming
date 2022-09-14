b5db3e842ed5b572d6bbaf40b5789bf4d9af8c5b
abiolaede <abiola2m4ac@gmail.com>
2022-09-13 18:23:19 -0700

    putchar added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * main-program that prints _putchar
- * Return: 0
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
 {
-	char c[8] = "_putchar";
-	int ch;
-	int i = 0;
-
-	while (c[i] = ch)
-	{
-		_putchar(ch);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
+
 }
