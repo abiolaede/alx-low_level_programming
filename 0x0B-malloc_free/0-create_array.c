b4b8be0725141e410862d53587ef97e9848d83aa
abiolaede <abiola2m4ac@gmail.com>
2022-10-03 17:13:50 -0700

    malloc added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @c: the content of the array
+ * @size: the size of the array
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int counter;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (counter = 0; counter < size; counter++)
+	{
+		*(array + counter) = c;
+	}
+	return (array);
+}
