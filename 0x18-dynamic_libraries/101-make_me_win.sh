14ea2ffd181bd891bb9a4370bb77bf5eb58e4181
abiolaede <abiola2m4ac@gmail.com>
2022-12-15 06:17:58 -0800

    make me win done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com./abiolaede/alx-low_level_programming/master/0x18-dynamic_libraries/bypass.so
+wget -P /tmp/ https://raw.github.com/abiolaede/alx-low_level_programming/master/0x18-dynamic_libraries/bypass.so
 export LD_PRELOAD=/tmp/bypass.so
